006e43d709022a07555eb7d72a89c7b95bab3e29
Cecilia Rodriguez <cecerodrgz@Cecilias-Air.lan>
2023-02-20 07:10:55 -0500

    typescript

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {
+  storiesReducer,
+  Item,
+  List,
+  SearchForm,
+  InputWithLabel, 
+} from './App'
+
+const storyOne = {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+};
+
+const storyTwo = {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+};
+
+const sotries = [storyOne, storyTwo];
+
+describe('storiesReducer', () => {
+  test('removes a story from all stories', () => {
+
+  });
+});
 
 test('renders learn react link', () => {
   render(<App />);
